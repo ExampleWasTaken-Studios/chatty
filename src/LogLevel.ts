24b124e6f09ec88a20844ac233471bbf1a6fdb37
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2022-12-09 09:06:06 +0100

    add log level presets

--- src/LogLevel.ts
+++ src/LogLevel.ts
@@ -1,1 +1,22 @@
 export type LogLevel = "fatal" | "error" | "warn" | "info" | "debug" | "trace" | "off" | "on";
+
+export const LogLevelPresets = {
+  all: [
+    "fatal",
+    "error",
+    "warn",
+    "info",
+    "debug",
+    "trace"
+  ],
+  production: [
+    "fatal",
+    "error",
+    "warn",
+    "info"
+  ],
+  development: [
+    "debug",
+    "trace"
+  ]
+};
